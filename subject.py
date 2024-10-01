9ecd944a6cf94e544ad13ce2f852304689548625
ypauli <yves.pauli@gmail.com>
2024-10-01 15:42:24 +0200

    minor

--- subject.py
+++ subject.py
@@ -1,10 +1,20 @@
 #class subject
 #==============
 
+from Document import Document
+
 class Subject:
     def __init__(self) -> None:
         
         #subject parameters...
         self.age = None
         self.gender = None
+
+        #specify all files belonging to same subject
+        self.files = []
+        if filelist:
+            for file in filelist:
+                self.files.append(file)
+
+        self.document1 = Document(self.files[0])
         #etc...
