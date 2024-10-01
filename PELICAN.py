cde158e5b022e3b7a29c60b7787e0ad172c1354f
ypauli <yves.pauli@gmail.com>
2024-10-01 15:07:48 +0200

    minor changes

--- PELICAN.py
+++ PELICAN.py
@@ -1,3 +1,13 @@
 #this is the main project file
 #created 1.10.24
 #=============================
+
+from subject import Subject
+
+if __name__ == '__main__':
+    #main script for preprocessing and extraction of data
+
+    for currentSubject in subjects:
+
+        #instantiate current subject
+        CurrentSubject = Subject()
