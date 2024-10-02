8d677d388131da7b7120d98753b440dbe14651e9
ypauli <yves.pauli@gmail.com>
2024-10-02 11:53:37 +0200

    framework setup

--- parameters.py
+++ parameters.py
@@ -5,3 +5,10 @@ PATH_FOLDER_TEXTFILES = 'sample_path' #set default to home directory
 language = 'german'     #possibly add options for german and english
 task1 = 'sampleTask'    #give name of task used for creation of text file (e.g. 'fluency')
 task2 = None            #optional: possible second task used for each subject, default: None
+
+desired_modifications = {
+    'removePunctuation': False,
+    'lowercase': False,
+    'removeSpecialCharacters': False
+    #etc...
+}
---
+++ text_processing.py
@@ -0,0 +1,3 @@
+def process_text(Document, parameter_dict):
+    #parameter_dict should contain desired text modifications
+    #e.g. removePunctuation = True
