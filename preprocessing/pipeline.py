d3c07e5fa98298832bbed43a5396e00698a62113
ypauli <yves.pauli@gmail.com>
2024-11-05 14:51:32 +0100

    first prototype

---
+++ preprocessing/pipeline.py
@@ -0,0 +1,23 @@
+from preprocessing.text_importer import TextImporter
+from preprocessing.text_cleaner import TextCleaner
+from preprocessing.text_tokenizer import TextTokenizer
+from preprocessing.text_normalizer import TextNormalizer
+
+class TextPreprocessingPipeline:
+    def __init__(self, config):
+
+        self.cleaner = TextCleaner(config.cleaning_options)
+        self.tokenizer = TextTokenizer(config.tokenization_options)
+        self.normalizer = TextNormalizer(config.normalization_options)
+
+    def process_document(self, document, is_dialog=False):
+        """
+        Processes a single document, handling individual chapters if present.
+        """
+        # Clean, tokenize, and normalize chapters or whole document
+        document.clean_text(self.cleaner, is_dialog=is_dialog)
+        document.tokenize_text(self.tokenizer)
+        document.normalize_text(self.normalizer)
+
+        # Return processed chapters if available, otherwise whole document
+        return document.get_processed_text()
--- preprocessing/text_processing.py
+++
@@ -1,3 +0,0 @@
-def process_text(Document, parameter_dict):
-    #parameter_dict should contain desired text modifications
-    #e.g. removePunctuation = True
