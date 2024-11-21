08bcc0d433e1c5b368725538711d5e6e1a540553
pucherem <pucherem@gmail.com>
2024-11-21 22:09:16 +0000

    Initial version

--- main.py
+++ main.py
@@ -1,51 +1,33 @@
-#this is the main project file
-#created 1.10.24
-#created by Yves Pauli
-#=============================
-import os
-import torch.cuda
-import sys
+from itertools import product
+import csv
+import time
+
 from config import Config
-from document import Document
-from preprocessing import Subject, Corpus
-from pathlib import Path
+from simu import Setup
+from simu import Generator
 
-if __name__ == '__main__':
-
-    #Initializing pipeline with configuration settings
+if __name__ == "__main__":
+    
     config = Config()
-    if config.gpu_available: torch.cuda.empty_cache()
+    setup = Setup(config)
+    
+    with open(config.file, "w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(config.parameters.keys())
+        
+        i = 1
+        
+        for parameter in setup.parameters:
+            print("Generation: ", i)
+            
+            start_time = time.time()
+            output, metrics = Generator(setup, config.device, parameter, config.constants).out
+            end_time = time.time()
 
-    #check/create LPDS (Language Processing Data Structure)
-    Path(config.PATH_TO_PROJECT_FOLDER + 'Outputs').mkdir(parents=True, exist_ok=True)
-    if not os.path.isdir(config.PATH_TO_SUBJECTS):
-        print('Warning: No Subjects folder')
-        sys.exit()
-
-    # Instantiate all subjects
-    subjects = []
-    for subject in os.listdir(config.PATH_TO_PROJECT_FOLDER+'Subjects/'):
-        subjects.append(Subject(subject))
-
-    for current_corpus in config.corpus_names:
-
-        print(f'{current_corpus} is being processed')
-
-        documents = []
-        # load all files belonging to same corpus
-        for i in range(len(subjects)):
-
-            #Check if subject has file belonging to corpus and add if available
-            FILEPATH = config.PATH_TO_PROJECT_FOLDER + 'Subjects/' + subjects[i].subjectID + '/' + current_corpus
-            if os.path.isdir(FILEPATH):
-                file_name = os.listdir(FILEPATH)[0]
-                documents.append(Document(FILEPATH, file_name, current_corpus))
-                subjects[i].add_document(documents[-1])
-
-        # Initialize the corpus with respective files
-        corpus = Corpus(current_corpus, documents, config)
-
-        # Process all files in the corpus
-        corpus.preprocess_all_documents()
-
-        #corpus.extract_logits()
+            print(f"Time taken for generation: {end_time - start_time:.2f} seconds")
+            print(parameter)
+            writer.writerow(parameter)
+            writer.writerow([metrics])
+            writer.writerow([output])  
+            writer.writerow(" ")             
+            i+=1
