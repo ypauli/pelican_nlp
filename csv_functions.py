b534bc12ed4f55ca7ca1d7134dbf0126a2faffff
ypauli <yves.pauli@gmail.com>
2024-11-29 12:56:16 +0100

    integration embeddings

--- csv_functions.py
+++ csv_functions.py
@@ -38,23 +38,3 @@ def store_features_to_csv(input_data, output_file):
         raise ValueError("Input must be a dictionary or a list.")
 
     print(f"Data successfully stored to {output_file}")
-
-
-'''
-def store_features_to_csv(per_token_data, results_csv_path):
-    # Write the data to the CSV file
-    if isinstance(per_token_data, list):
-        headers = per_token_data[0].keys()
-        with open(results_csv_path, mode='w', newline='', encoding='utf-8') as csv_file:
-            writer = csv.DictWriter(csv_file, fieldnames=headers)
-            writer.writeheader()
-            writer.writerows(per_token_data)
-    elif isinstance(per_token_data, dict):
-        headers = per_token_data.keys()
-        with open(results_csv_path, mode='w', newline='', encoding='utf-8') as csv_file:
-            writer = csv.DictWriter(csv_file, fieldnames=headers)
-            writer.writeheader()
-            writer.writerow(per_token_data)
-    print(f"Data successfully saved to {results_csv_path}")
-    return
-'''
