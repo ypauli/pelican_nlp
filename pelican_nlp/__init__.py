d0219028019d24e9a832793595faa139f28913a4
ypauli <yves.pauli@gmail.com>
2025-04-07 14:04:55 +0200

    TestPyPI troubleshoot

--- pelican_nlp/__init__.py
+++ pelican_nlp/__init__.py
@@ -1,22 +1,9 @@
-from .main import Pelican  # Now importing Pelican
-
-from .preprocessing import LPDS
-from .preprocessing import TextCleaner
-from .preprocessing import TextImporter
-from .preprocessing import TextTokenizer
-from .preprocessing import TextNormalizer
-from .preprocessing import TextPreprocessingPipeline
-
-from .core.corpus import Corpus
-from .core.document import Document
-from .core.subject import Subject
-from .core.audio_document import AudioFile
-
-from .extraction.extract_embeddings import EmbeddingsExtractor
-from .extraction.extract_acoustic_features import AudioFeatureExtractor
-from .extraction.extract_logits import LogitsExtractor
-
 # Version and metadata
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Yves Pauli"
+
+try:
+    from .main import Pelican
+except ImportError as e:
+    print(f"Warning: Could not import Pelican class: {e}")
 
